5b628a875c3de7ea078197073031ddbb1ab5d7d2
Ji Chang <jichang_dev@outlook.com>
2024-11-29 04:53:04 +0000

    update implementation of text rect

--- packages/react/src/components/pageLayers/text.tsx
+++ packages/react/src/components/pageLayers/text.tsx
@@ -1,10 +1,12 @@
 import {
   ignore,
   PdfErrorCode,
+  PdfPageObject,
   PdfTextRectObject,
+  Rotation,
   transformRect,
 } from '@unionpdf/models';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { usePdfEngine, usePdfDocument } from '../../core';
 import { PdfPageLayerComponentProps } from './layer';
 import './text.css';
@@ -43,41 +45,55 @@ export function PdfPageTextLayer(props: PdfPageTextLayerProps) {
     <div className="pdf__page__layer pdf__page__layer--text">
       {isVisible
         ? rects.map((textRect, index) => {
-            const rect = transformRect(
-              page.size,
-              textRect.rect,
-              rotation,
-              scaleFactor,
-            );
-            const style = {
-              top: rect.origin.y,
-              left: rect.origin.x,
-              width: rect.size.width,
-              height: rect.size.height,
-            };
-
             return (
-              <span className="pdf__text__rect" key={index} style={style}>
-                <svg
-                  width={style.width}
-                  height={style.height}
-                  viewBox={`0 0 ${style.width} ${style.height}`}
-                >
-                  <text
-                    y={style.height}
-                    fontSize={style.height}
-                    textLength={style.width}
-                    lengthAdjust="spacingAndGlyphs"
-                    fill="transparent"
-                    height={style.height}
-                  >
-                    {textRect.content}
-                  </text>
-                </svg>
-              </span>
+              <PdfPageTextRect
+                key={index}
+                page={page}
+                textRect={textRect}
+                rotation={rotation}
+                scaleFactor={scaleFactor}
+              />
             );
           })
         : null}
     </div>
+  );
+}
+
+export interface PdfPageTextRectProps {
+  page: PdfPageObject;
+  textRect: PdfTextRectObject;
+  rotation: Rotation;
+  scaleFactor: number;
+}
+
+export function PdfPageTextRect(props: PdfPageTextRectProps) {
+  const { page, textRect, rotation, scaleFactor } = props;
+  const rect = transformRect(page.size, textRect.rect, rotation, scaleFactor);
+  const style = {
+    top: rect.origin.y,
+    left: rect.origin.x,
+    width: rect.size.width,
+    height: rect.size.height,
+  };
+
+  const textElemRef = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    if (textElemRef.current) {
+      const contentElem = textElemRef.current;
+      const width = contentElem.offsetWidth;
+      const height = contentElem.offsetHeight;
+
+      contentElem.style.transform = `scale(${rect.size.width / width}, ${rect.size.height / height})`;
+    }
+  }, [rect]);
+
+  return (
+    <span className="pdf__text__rect" style={style}>
+      <span className="pdf__text__rect__content" ref={textElemRef}>
+        {textRect.content}
+      </span>
+    </span>
   );
 }
