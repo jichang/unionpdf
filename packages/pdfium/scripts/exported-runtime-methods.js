68b40174cba9c21667591231b926a259adc8a13f
nguyenyou <tunt081295@gmail.com>
2025-06-29 20:45:53 +0100

    PDFium 7267

--- packages/pdfium/scripts/exported-runtime-methods.js
+++ packages/pdfium/scripts/exported-runtime-methods.js
@@ -8,8 +8,12 @@ exports.methods = [
   'UTF16ToString',
   'stringToUTF8',
   'stringToUTF16',
+  'HEAP8',
+  'HEAPU8',
 ];
 
 exports.types = {
   wasmExports: 'WasmExports',
+  HEAP8: 'Int8Array',
+  HEAPU8: 'Uint8Array',
 };
